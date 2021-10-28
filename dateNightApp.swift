c74fbf277e3a4e970b38985f3f3a12bbdbfa43b2
longmach <54420477+longmach@users.noreply.github.com>
2021-10-27 21:16:09 -0400

    DateNight_App version 1.0.0

---
+++ dateNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  dateNightApp.swift
+//  dateNight
+//
+//  Created by MAC on 2021-10-27.
+//
+
+import SwiftUI
+
+@main
+struct dateNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
